3d3e58b29a614e2f93f002a625b6466ebf16b472
Seif Jelidi <seifjelidi3@gmail.com>
2020-09-10 15:43:52 +0000

    fix states controller

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,8 @@ init module
 """
 
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 app_views = Blueprint("app_views", __name__)
+
+from api.v1.views.index import *
+from api.v1.views.states import *
