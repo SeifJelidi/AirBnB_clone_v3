6ddac0bf0420d89ea9c10dcee65de45f49302f54
Seif Jelidi <seifjelidi3@gmail.com>
2020-09-10 14:32:31 +0000

    count objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Handling routes
 """
 
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 
@@ -13,3 +14,22 @@ def status_ok():
     Return the status of the API:
     """
     return jsonify(status="OK")
+
+
+@app_views.route("/stats")
+def stats_count():
+    """
+    retrieves the number of each objects by type
+    """
+    types = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+        }
+    dict_count = {}
+    for k, t in types.items():
+        dict_count[t] = storage.count(k)
+    return jsonify(dict_count)
